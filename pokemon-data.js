774ea1a71ecc9909c6578c2936675985f41195d6
randomboy1458 <randomboy1458@gmail.com>
2025-12-02 12:26:39 +0530

    Add Pokémon data file with all 25 Pokémon

---
+++ pokemon-data.js
@@ -0,0 +1,40 @@
+﻿// pokemon-data.js - 25 Pokémon Data (MATCHES levels.js EXACTLY)
+const POKEMON_DATA = {
+    // Room 1 Pokémon
+    1: { name: "Pikachu", sprite: "sprites/pokemon/pkm1.png" },
+    2: { name: "Charizard", sprite: "sprites/pokemon/pkm2.png" },
+    3: { name: "Squirtle", sprite: "sprites/pokemon/pkm3.png" },
+    4: { name: "Bulbasaur", sprite: "sprites/pokemon/pkm4.png" },
+    5: { name: "Emboar", sprite: "sprites/pokemon/pkm5.png" },
+    
+    // Room 2 Pokémon
+    6: { name: "Meowth", sprite: "sprites/pokemon/pkm6.png" },
+    7: { name: "Eevee", sprite: "sprites/pokemon/pkm7.png" },
+    8: { name: "Snorlax", sprite: "sprites/pokemon/pkm8.png" },
+    9: { name: "Mew", sprite: "sprites/pokemon/pkm9.png" },
+    10: { name: "Vulpix", sprite: "sprites/pokemon/pkm10.png" },
+    
+    // Room 3 Pokémon
+    11: { name: "Psyduck", sprite: "sprites/pokemon/pkm11.png" },
+    12: { name: "Lucario", sprite: "sprites/pokemon/pkm12.png" },
+    13: { name: "Poliwag", sprite: "sprites/pokemon/pkm13.png" },
+    14: { name: "Abra", sprite: "sprites/pokemon/pkm14.png" },
+    15: { name: "Machamp", sprite: "sprites/pokemon/pkm15.png" },
+    
+    // Room 4 Pokémon
+    16: { name: "Fuecoco", sprite: "sprites/pokemon/pkm16.png" },
+    17: { name: "Gastly", sprite: "sprites/pokemon/pkm17.png" },
+    18: { name: "Dratini", sprite: "sprites/pokemon/pkm18.png" },
+    19: { name: "Chikorita", sprite: "sprites/pokemon/pkm19.png" },
+    20: { name: "Cyndaquil", sprite: "sprites/pokemon/pkm20.png" },
+    
+    // Room 5 Pokémon
+    21: { name: "Xerneas", sprite: "sprites/pokemon/pkm21.png" },
+    22: { name: "Ho-Oh", sprite: "sprites/pokemon/pkm22.png" },
+    23: { name: "Amaura", sprite: "sprites/pokemon/pkm23.png" },
+    24: { name: "Diglett", sprite: "sprites/pokemon/pkm24.png" },
+    25: { name: "Hoppip", sprite: "sprites/pokemon/pkm25.png" }
+};
+
+// Final Gift Message
+const FINAL_GIFT_MESSAGE = "Your final romantic gift message here...";
